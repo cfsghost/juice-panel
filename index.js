0c50f9000b64c2a4f130fd833905e79ec84dc533
Fred Chien <fred@mandice.com>
2012-06-05 03:17:14 +0800

    add a logo

--- index.js
+++ index.js
@@ -46,6 +46,33 @@ function initApplication(settings, callback) {
 			window.x = 0;
 			window.y = 0;
 			window.show();
+
+			/* Initializing Layout */
+			var layout = new toolkit.Group;
+			window.add(layout);
+
+			/* Logo */
+			var logo = new toolkit.Text('JUICE');
+			logo.setFontName('Droid Sans 72');
+			logo.setColor(255, 255, 255, 255);
+			logo.setAnchorFromGravity(toolkit.GRAVITY_CENTER);
+			logo.x = window.width * 0.5;
+			logo.y = window.height * 0.5;
+			logo.scale(0.5, 0.5);
+			logo.opacity = 0;
+//			logo.rotate(0, toolkit.GRAVITY_CENTER);
+			layout.add(logo);
+
+			/* Animation */
+			logo.animate(toolkit.EASE_OUT_CUBIC, 1000, {
+				'scale-x': 1.0,
+				'scale-y': 1.0,
+				'opacity': 255
+			}, function() {
+				logo.animate(toolkit.EASE_OUT_CUBIC, 2000, {
+					'rotation-angle-y': 360
+				}, { loop: true });
+			});
 		});
 
 		/* Initailizing panel */
